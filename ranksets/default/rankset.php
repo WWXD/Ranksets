5fe205ae4ad45300b1b8de6d99d2c5a4b535fe5e
maorninja <maorninja@outlook.com>
2016-10-21 08:48:59 -0700

    Added the ranksets
    Move to another rep.

---
+++ ranksets/default/rankset.php
@@ -0,0 +1,12 @@
+<?php
+
+$ranksetNames["mario"] = "Mario";
+
+$ranksetData["mario"] = array(
+	array("num" => 0, "image" => '', "text" => 'Nobody'),
+	array("num" => 1, "image" => '', "text" => 'Random nobody'),
+	array("num" => 10, "image" => '', "text" => 'User'),
+	array("num" => 25, "image" => '', "text" => 'Member'),
+	array("num" => 1000, "image" => '', "text" => 'Catgirl'),
+	array("num" => 2500, "image" => '', "text" => 'Common spammer')
+);
